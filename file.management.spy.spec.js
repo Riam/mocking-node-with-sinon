8f3816bc44f402d1008a73a9586919fe149a1454
Riam Djadri <rdjadri@regmail.west.com>
2021-01-21 15:27:49 -0500

    Add unit tests with spies Add proxywire to inject spied fs instance

--- file.management.spy.spec.js
+++ file.management.spy.spec.js
@@ -1,11 +1,47 @@
 const { expect } = require("chai");
-const { createSandbox, spy } = require("sinon");
+const sinon = require("sinon");
 const fs = require("fs");
 const proxyquire = require("proxyquire");
 const fileManagement = require("./file.management");
 
 describe("File Management", () => {
-  it("true should be true", () => {
-    expect(true).to.be.true;
+  afterEach( () => {
+    sinon.restore();
+  });
+  
+  describe("When writing a new new file", () => {
+    it("should call writeFileSync", () => {
+      const writeSpy = sinon.spy(fs, "writeFileSync");
+      const fileManagement = proxyquire("./file.management", { fs });
+
+      fileManagement.createFile("test.txt");
+
+      expect(writeSpy.calledWith("./data/test.txt", "")).to.be.true;
+    });
+
+    it("should not write a new file if no file name is specified", () => {
+      const writeSpy = sinon.spy(fs, "writeFileSync");
+      const fileManagement = proxyquire("./file.management", { fs });
+
+      try {
+        fileManagement.createFile();
+
+      } catch (error) {
+
+      }
+
+      expect(writeSpy.notCalled).to.be.true;
+    });
+
+    it.skip("should call writeFileSync = Injected", () => {
+      const writeSpy = spy(fs, "writeFileSync");
+      fileManagement.createFileInjected("test.txt", fs);
+
+      // expect(writeSpy.calledOnce).to.be.true;
+      expect(writeSpy.calledWith("./data/test.txt", "")).to.be.true;
+    });
+
+
+
   });
 });
